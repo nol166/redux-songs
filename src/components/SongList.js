b9ba27f8b75f879d2f9f5cfe3b19f82b245dca5e
John McCambridge <jmccamb@gmail.com>
2020-03-16 12:24:39 -0500

    add materialize, updated npm package version

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux' // always going to import connect
 
 class SongList extends Component {
+    // this.props === { songs: state.songs}
     render() {
+        console.log(this.props.songs)
+
         return <div>SongList</div>
     }
 }
 
-const mapStateToProps = state => {}
+/**
+ * Always going to define mapStateToProps
+ * Always given a first argument of state
+ * Always going to return an object that is
+ * going to show up as "props" inside component
+ */
+const mapStateToProps = state => {
+    console.log(state)
 
-export default connect()(SongList)
+    return { songs: state.songs }
+}
+
+/**
+ * always going to call connect and
+ * pass in component (SongList) as the second function called
+ */
+export default connect(mapStateToProps)(SongList)
